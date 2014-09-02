e34a7dad4a53ba6a2d2b5292b57325046e444569
yukke <soh.ishiguro3914@gmail.com>
2014-09-02 20:26:00 +0900

    Traceback method

--- component_interaction.py
+++ component_interaction.py
@@ -1,9 +1,13 @@
 from ecocyc_parser import EcocycParser
+from operator import add
+import itertools
+import pprint 
 
 __author__  = 'Soh ISHIGURO'
 __email__   = 'si914@sfc.keio.ac.jp'
 __license__ = 'Not yet'
 
+pp = pprint.PrettyPrinter()
 
 class ComponentInteraction(object):
     def __init__(self):
@@ -22,22 +26,24 @@ class ModifiedProteinInteraction(ComponentInteraction):
         ComponentInteraction.__init__(self)
         self.proteins_db = proteins_db
 
-    def modified_proteins(self):
+    def traceback_to_unmodified_proteins(self):
         for p_id in self.proteins_db:
-            #self.proteins_db[p_id].get("UNMODIFIED-FORM")
-            #print self.proteins_db[p_id]
-            types =  self.proteins_db[p_id].get("TYPES") # == "Modified-Proteins"
-            print types
-            #print self.proteins_db[p_id]
-            #print p_id
+            types = self.proteins_db[p_id].get("TYPES")
 
+            # search "unmodified" protein from "modified" protein
+            if types is not None and "Modified-Proteins" in types:
+                unmodified_p = self.proteins_db[p_id]["UNMODIFIED-FORM"]
+                if unmodified_p[0] in [ pool for pool in self.proteins_db ]:
+                    only_type = next(itertools.ifilter(lambda x: "Modified-Proteins" not in x, types), None)
+                    print "Modified:'{}' => Original:'{}'".format(only_type, ",".join(unmodified_p))
+                    
+    def traceback_to_modified_proteins(self):
+        pass
+                
             
 if __name__ == '__main__':
     proteins_dat  = '/Users/yukke/dev/ecellp2014/ecocyc/data/proteins.dat'
     ecoparser = EcocycParser()
     proteins_db = ecoparser.generate_proteins_entory(proteins_dat)
     interaction = ModifiedProteinInteraction(proteins_db)
-    print interaction.modified_proteins()
-
-
-    
+    print interaction.traceback_to_unmodified_proteins()
