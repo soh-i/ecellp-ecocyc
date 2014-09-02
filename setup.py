d590aaba710be5f7a9d82acc98def6c0dc4729f6
yukke <soh.ishiguro3914@gmail.com>
2014-09-02 23:32:13 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import sys
+
+packages=['ecellp-ecocyc']
+
+setup(
+    name='ecellp',
+    version='1.0',
+    description='',
+    author='Soh Ishiguro',
+    packages=packages
+)
